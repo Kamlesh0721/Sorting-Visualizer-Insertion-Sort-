655ee03f4a6e2f29b5851dedd0d85519539a7709
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-08-16 02:41:31 +0530

    add bars in panel

--- src/Components/Visualizer.java
+++ src/Components/Visualizer.java
@@ -6,10 +6,32 @@ import java.awt.*;
 public class Visualizer extends JPanel {
     private final int WIDTH = 1000;
     private final int HEIGHT = WIDTH * 9 / 16;
+    private final int SIZE = 200;
+    private final int BAR_WIDTH = WIDTH / SIZE;
+    private final float[] barHeight = new float[SIZE];
 
     private Visualizer() {
         setBackground(Color.BLACK);
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        initBarHeight();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.CYAN);
+        for (int i = 0; i < SIZE; i++) {
+            g.drawRect(i * BAR_WIDTH, 0, BAR_WIDTH, (int) barHeight[i]);
+        }
+
+    }
+
+    private void initBarHeight() {
+        float interval = (float) HEIGHT / SIZE;
+        for (int i = 1; i <= SIZE; i++) {
+            barHeight[i - 1] = i * interval;
+        }
     }
 
     public static void main(String[] args) {
@@ -20,7 +42,7 @@ public class Visualizer extends JPanel {
             frame.setContentPane(new Visualizer());
             frame.validate();
             frame.pack();
-           frame.setLocationRelativeTo(null); //Set Frame at Centre of Screen
+            frame.setLocationRelativeTo(null); //Set Frame at Centre of Screen
             frame.setVisible(true);
 
         });
